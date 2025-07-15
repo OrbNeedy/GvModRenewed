ca1cc78882812955aa0b7a5c8fcb7149e0df00cc
OrbNeedy <theviod36@gmail.com>
2025-07-15 18:55:56 -0500

    Added the protective armor set.

---
+++ Content/Items/Armor/Protective/ProtectiveBody.cs
@@ -0,0 +1,53 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace GvMod.Content.Items.Armor.Protective
+{
+    [AutoloadEquip(EquipType.Body)]
+    public class ProtectiveBody : ModItem
+    {
+        private float increaseInSeptimaDamage = 5;
+        public LocalizedText SetBonusText { get; private set; }
+
+        public override void SetDefaults()
+        {
+            Item.value = Item.sellPrice(0, 0, 10, 0);
+            Item.rare = ItemRarityID.Blue;
+            Item.defense = 6;
+
+            SetBonusText = this.GetLocalization("SetBonus").WithFormatArgs(increaseInSeptimaDamage);
+        }
+
+        public override LocalizedText Tooltip => base.Tooltip.WithFormatArgs(increaseInSeptimaDamage);
+
+        public override void UpdateEquip(Player player)
+        {
+            float increase = increaseInSeptimaDamage / 100f;
+            player.GetDamage<SeptimaDamage>() += increase;
+        }
+
+        public override bool IsArmorSet(Item head, Item body, Item legs)
+        {
+            return legs.type == ModContent.ItemType<ProtectiveLegs>();
+        }
+
+        public override void UpdateArmorSet(Player player)
+        {
+            float increase = increaseInSeptimaDamage / 100f;
+            player.setBonus = SetBonusText.Value;
+            player.GetDamage<SeptimaDamage>() += increase;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddRecipeGroup("IronBar", 7)
+                .AddRecipeGroup("GoldBar", 5)
+                .AddIngredient(ItemID.Silk, 4)
+                .AddTile(TileID.Anvils)
+                .Register();
+        }
+    }
+}
---
+++ Content/Items/Armor/Protective/ProtectiveLegs.cs
@@ -0,0 +1,33 @@
+﻿using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace GvMod.Content.Items.Armor.Protective
+{
+    [AutoloadEquip(EquipType.Legs)]
+    public class ProtectiveLegs : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.value = Item.sellPrice(0, 0, 10, 0);
+            Item.rare = ItemRarityID.Blue;
+            Item.defense = 5;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddRecipeGroup("IronBar", 5)
+                .AddRecipeGroup("GoldBar", 3)
+                .AddIngredient(ItemID.Silk, 5)
+                .AddTile(TileID.Anvils)
+                .Register();
+        }
+    }
+}
